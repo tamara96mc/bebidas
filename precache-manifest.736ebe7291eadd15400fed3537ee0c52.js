03c35cc884c1fce65f02f5c30ba85699324db989
tamara96mc <tamara.96mc@gmail.com>
2020-12-23 10:10:04 +0100

    Updates

---
+++ precache-manifest.736ebe7291eadd15400fed3537ee0c52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba814ac6b777c227ec82afd859001683",
+    "url": "/bebidas/index.html"
+  },
+  {
+    "revision": "4b0a441f7fdb6bb0f44c",
+    "url": "/bebidas/static/css/main.55c35015.chunk.css"
+  },
+  {
+    "revision": "d8d1abcb8c0809e5f2c9",
+    "url": "/bebidas/static/js/2.ba6d0e25.chunk.js"
+  },
+  {
+    "revision": "8f75fe0532614f1d4d17c26594b021fc",
+    "url": "/bebidas/static/js/2.ba6d0e25.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4b0a441f7fdb6bb0f44c",
+    "url": "/bebidas/static/js/main.6abddea9.chunk.js"
+  },
+  {
+    "revision": "e53b272916405be40c21",
+    "url": "/bebidas/static/js/runtime-main.b9526bdc.js"
+  }
+]);
